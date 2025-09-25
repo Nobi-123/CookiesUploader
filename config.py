9c9f4f7015c79fdaef9b0c40a103963419451277
Nobi-123 <nobita.infringement.report@gmail.com>
2025-09-25 23:06:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
-LOGGER_ID = int(os.getenv("-1003065367480"))
+LOGGER_ID = -1003065367480
